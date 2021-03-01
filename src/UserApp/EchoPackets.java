43de4cb7931dfd45fef9fe94e4320c48b508d9d6
Vasilis Kyriafinis <bkyriafinis@gmail.com>
2021-03-02 00:14:25 +0200

    Implemented the interface for the Echo module
    Added save to file functionality
    Added end of transmission detection
    Added function that creates unique file name
    Made a few changes to the class attributes

--- src/UserApp/EchoPackets.java
+++ src/UserApp/EchoPackets.java
@@ -1,24 +1,54 @@
 package UserApp;
 
+import Stracture.DataPackets;
+
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
-public class EchoPackets {
+public class EchoPackets implements DataPackets {
 
     private final Connection connection;
     private final List<String> echo_packets;
+    private int default_packet_number;
+    private final StringBuilder packet = new StringBuilder();  // Complete packet
 
     /**
      * Constructor
      * @param connection the connection object
+     * @param default_packet_number  the number of the packets to be received
      */
-    public EchoPackets(Connection connection){
+    public EchoPackets(Connection connection, int default_packet_number){
         this.connection = connection;
+        this.default_packet_number = default_packet_number;
 
         // initialize the echo packets list
         this.echo_packets = new ArrayList<>();
     }
 
+
+
+    // ===============================  Getters - Setters  ===============================
+
+    public List<String> getEcho_packets() {
+        return echo_packets;
+    }
+
+    public int getDefault_packet_number() {
+        return default_packet_number;
+    }
+
+    public void setDefault_packet_number(int default_packet_number) {
+        this.default_packet_number = default_packet_number;
+    }
+
+
+
+    // ===============================  Interface methods  ===============================
 
     /**
      * Receives large number of echo packets from the server. The packets are:
@@ -30,16 +60,17 @@ public class EchoPackets {
      *     HH-MM-SS time of send
      *     PC packet counter modulo(100)
      *
-     * @param packet_counter The number of packets to be requested
-     *
      */
-    public void getEchoPackets(int packet_counter) {
+    @Override
+    public void getPackets() {
         int k;  // The input buffer byte
-        StringBuilder packet = new StringBuilder();  // Complete packet
 
-        for (int i = 0; i <= packet_counter; i++) {
+        System.out.println("Receiving echo packets ...");
+
+        for (int i = 0; i < this.default_packet_number; i++) {
             // Request the echo_packet
             if (this.connection.getModem().write(this.connection.getEcho_code().getBytes())) {
+
                 while (true) {
                     try {
                         // Read the bytes
@@ -52,10 +83,10 @@ public class EchoPackets {
                         }
 
                         // Append to the packet string
-                        packet.append((char) k);
+                        this.packet.append((char) k);
 
                         // Detect end of packet
-                        if (this.connection.isMessageOver(packet.toString(), Commands.PACKET_END.getStr())) break;
+                        if (isTransmissionOver()) break;
 
                     } catch (Exception x) {
                         System.out.println("Exception thrown: " + x.toString());
@@ -63,7 +94,7 @@ public class EchoPackets {
                     }
                 }
 
-                System.out.println("Packet received: " + packet.toString());
+                //System.out.println("Packet received: " + packet.toString());  // DEBUG comment
 
                 // Add packet to the list
                 this.echo_packets.add(packet.toString());
@@ -76,5 +107,70 @@ public class EchoPackets {
                 break;
             }
         }
+
+        // Save packets to a file
+        saveToFile(createFileName());
+    }
+
+    /**
+     * Compares the packet's (at the current state) end with the end packet string
+     * @return True if the pattern matched false if not or if the message was too short
+     */
+    @Override
+    public boolean isTransmissionOver() {
+        String packet_end = Commands.PACKET_END.getStr();
+        if (this.packet.length() < packet_end.length()) {
+            return false;
+        } else return this.packet.toString().endsWith(packet_end);
+    }
+
+    /**
+     * Save all the echo packets form the list to a file
+     * @param file_name the name of the file
+     */
+    @Override
+    public void saveToFile(String file_name) {
+        try {
+            FileWriter writer = new FileWriter(file_name);
+
+            writer.write("## " + this.connection.getEcho_code() + " " + this.connection.getImage_code() + " " +
+                    this.connection.getImage_code_error() + " " + this.connection.getGps_code() + " " +
+                    this.connection.getAck_result_code() + " " + this.connection.getNack_result_code() + " ##" +
+                    System.lineSeparator());
+
+            for(String str: this.echo_packets) {
+                writer.write(str + System.lineSeparator());
+            }
+            writer.close();
+
+            this.echo_packets.clear();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Gets the current date time and formats it "yyyy-MM-dd HH-mm-ss"
+     * @return name + date + .txt
+     */
+    @Override
+    public String createFileName() {
+        // Create the name of the image file depending on the errors
+        String name = "EchoPackets/echo_packets ";
+
+        String pattern = "yyyy-MM-dd HH-mm-ss";
+
+        // Create an instance of SimpleDateFormat used for formatting
+        // the string representation of date according to the chosen pattern
+        DateFormat df = new SimpleDateFormat(pattern);
+
+        // Get the today date using Calendar object.
+        Date today = Calendar.getInstance().getTime();
+
+        // Using DateFormat format method we can create a string
+        // representation of a date with the defined format.
+
+        return name + df.format(today) + ".txt";
     }
 }
