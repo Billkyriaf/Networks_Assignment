03d57d41d443e9bd29e1033f2d97fae6cb9ba85d
Vasilis Kyriafinis <bkyriafinis@gmail.com>
2021-04-01 17:20:34 +0300

    Created tests for all the modules of the project

--- Networks_Assignment/src/UserApp/userApplication.java
+++ Networks_Assignment/src/UserApp/userApplication.java
@@ -6,6 +6,10 @@ import Echo.EchoPackets;
 import GPS.GPSPackets;
 import Image.ImagePackets;
 import Structure.Connection;
+import org.checkerframework.checker.units.qual.A;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class userApplication {
 
@@ -31,37 +35,81 @@ public class userApplication {
          * IMPORTANT NOTE!!  for the GPS module a 4 second delay is used at some points if the timeout parameter is
          * less that 5 the connection will time out.
          */
-        Connection connection = new Connection(args[0], args[1], args[2], args[3], args[4], args[5], 76000, 10000);
+        Connection connection = new Connection(args[0], args[1], args[2], args[3], args[4], args[5], 80000, 10000);
 
-        // Echo packets
+
+        // Comment or uncomment one or more of the following sections to run the tests you like!!
+
+        // ===================================   Echo packets   ===================================
         EchoPackets echoPackets = new EchoPackets(connection, 5000);
+        connection.setModemSpeed(3800);
 
-        // Image packets
+        // Get 5 minutes of echo packets
+        echoPackets.getPackets();
+
+
+
+        // ===================================   Echo packets with errors   ===================================
+        EchoErrors echoErrors = new EchoErrors(connection, 5000);
+        connection.setModemSpeed(3800);
+
+        // Get 5 minutes of echo packets with errors
+        echoErrors.getPackets();
+
+
+
+        // ===================================   Image   ===================================
         ImagePackets clear_image = new ImagePackets(connection, false);
+        connection.setModemSpeed(80000);
 
+        // Get single image
+        clear_image.getPackets();
+
+        // Gets 10 images with the camera rotating left
+        clear_image.setCamera_commands("CAM=PTZ DIR=L");
+        for (int i = 0; i < 10; i++)
+            clear_image.getPackets();
+
+
+
+        // ===================================   Corrupted image   ===================================
         ImagePackets corrupted_image = new ImagePackets(connection, true);
+        connection.setModemSpeed(80000);
 
-        // GPS packets
-        GPSPackets gpsData = new GPSPackets(connection, "R=9050099");
-
-        EchoErrors echoErrors = new EchoErrors(connection, 1000);
-
-        int num = 10;
+        // Get a corrupted image
+        corrupted_image.getPackets();
 
 
-        // Run some tests
-        //echoPackets.getPackets();
 
-//        clear_image.setCamera_commands("CAM=PTZ DIR=L");
-//        for (int i = 0; i < 10; i++)
-//            clear_image.getPackets();
+        // ===================================   GPS data   ===================================
+        GPSPackets gpsData = new GPSPackets(connection, "R=1000199");
+        connection.setModemSpeed(80000);
 
-        //corrupted_image.getPackets();
-
+        // Get the gps data and the gps image
         gpsData.getPackets();
 
-        //echoErrors.getPackets();
 
-        connection.getModem().close();
+
+        // ===================================    Get the hole route 1   ===================================
+        ArrayList<String> codes = new ArrayList<>(Arrays.asList("R=1000199", "R=1010099", "R=1020099", "R=1030099",
+                "R=1040099", "R=1050099", "R=1060099", "R=1070099", "R=1080099", "R=1090099", "R=1100099"));
+
+        for (String code: codes) {
+            // GPS packets
+            GPSPackets gpsRoute = new GPSPackets(connection, code);
+            gpsRoute.getPackets();
+        }
+
+
+
+
+        // Finally close the connection with the server
+        try {
+            connection.getModem().close();
+        }
+        catch (Exception e){
+            System.out.println("Exception caught while trying to close the modem connection");
+        }
+
     }
 }
